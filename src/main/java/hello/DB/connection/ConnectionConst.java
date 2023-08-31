48664b0b94e4e0ca94bc42b27b5a98fe035b0be0
dorameme <doradorav@naver.com>
2023-08-31 14:31:12 +0900

    설정과 분리

--- src/main/java/hello/DB/connection/ConnectionConst.java
+++ src/main/java/hello/DB/connection/ConnectionConst.java
@@ -5,4 +5,5 @@ public abstract class ConnectionConst {//객체생성을 막아둠.
     public static final String USERNAME="sa";
     public static final String PASSWORD="";
 }
-///DB커넥션에 대한 정보를 넣어줌
+///DB커넥션에 대한 정보를 넣어줌//
+///Users/kimyunsoo/Downloads/h2 3 경로
